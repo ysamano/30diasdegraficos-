7691ca45af2b760d615368f978ac07efc87c1cde
Yobanny Samano <yobanny_sc@hotmail.com>
2020-06-27 14:05:03 -0500

    commit

--- dia_02/dia2_lineas.R
+++ dia_02/dia2_lineas.R
@@ -1,7 +1,6 @@
 library(tidyverse)
 library(ggtext)
 
-
 esp_vida <- readxl::read_excel("dia_02/esperanza_vida.xlsx")
 
 esperanza_tidy <- esp_vida %>% 
@@ -12,11 +11,11 @@ esperanza_tidy <- esp_vida %>%
                              TRUE ~ "Otros"))
 
 
-x11()
-ggplot(esperanza_tidy, aes(anios, valores, group = pais)) +
+p1 <- 
+  ggplot(esperanza_tidy, aes(anios, valores, group = pais)) +
   geom_line(size = 0.1, colour = "gray70") +
   geom_line(data = esperanza_tidy %>% filter(pais %in% c("Camboya", "Rwanda")),
-            aes(anios, valores, group = pais,  colour = colores), size = 0.6) +
+            aes(anios, valores, group = pais,  colour = colores), size = 0.7) +
   scale_x_continuous(breaks = seq(1960, 2020, by = 10)) +
   scale_y_continuous(breaks = seq(0, 90, by = 20), limits = c(0, 90)) +
   scale_color_manual(values = c("#ffc50c", "#ec640f")) +
@@ -24,24 +23,13 @@ ggplot(esperanza_tidy, aes(anios, valores, group = pais)) +
        <span style='color:#ffc50c'>**Camboya**</span> y <span style='color:#ec640f'>**Rwanda**</span>", 
        caption = "Fuente: Banco Mundial",
        x = "Año",
-       y = "Esperanza de vida de mujeres en años") + 
-  theme_minimal(base_size = 7, base_family = "Roboto Condensed Light") +
-  theme(plot.background = element_rect(fill = "#1b1f2b", color = "#1b1f2b"),
-        plot.title = element_markdown(family = "Roboto Condensed", colour = "white",
-                                  size = 12,  margin = margin(t = 5, b = 10)),
-        plot.caption = element_text(size = 6, colour = "white"),
-        panel.grid.major.x = element_line(colour = "gray30", size = 0.1),
-        panel.grid.minor.x = element_line(colour = "gray30", size = 0.1),
-        panel.grid.major.y = element_line(colour = "gray30", size = 0.1),
-        panel.grid.minor.y = element_line(colour = "gray30", size = 0.1),
-        axis.title = element_text(colour = "gray90", hjust = 1),
-        axis.text = element_text(colour = "gray90"),
-        legend.position = "none")
+       y = "Esperanza de vida de mujeres (años)") + 
+  theme_ybn_b(base_size = 11) +
+  theme(legend.position = "none",
+        plot.title = element_markdown(family = "Roboto Condensed",
+                                      colour = "white",
+                                      size = 25,
+                                      hjust = 0.5))
 
-ggsave("dia_02/02_lineas.png", height = 7, width = 5, units = "in", dpi = 400)
-
-
-
-
-
+ggsave("dia_02/02_lineas2.png", p1, height = 11, width = 8.5, units = "in", dpi = 400)
 
