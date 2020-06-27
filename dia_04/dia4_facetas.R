9d93fcc4138ef773a58debd1dee53aee221d829a
Yobanny Samano <yobanny_sc@hotmail.com>
2020-06-27 15:53:46 -0500

    commit

--- dia_04/dia4_facetas.R
+++ dia_04/dia4_facetas.R
@@ -1,5 +1,4 @@
 library(tidyverse)
-extrafont::loadfonts("win")
 
 pob_pobreza <- readxl::read_excel("dia_04/pob_pobreza.xlsx")
 
@@ -7,41 +6,31 @@ pobreza_tidy <- pob_pobreza %>%
   pivot_longer(cols = 2:7, names_to = "anio", values_to = "porcentaje") %>% 
   mutate(anio = as.double(anio))
 
-x11()
 p1 <- 
-  ggplot(pobreza_tidy, aes(x = anio)) + 
-  geom_ribbon(aes(ymin = 0, ymax = porcentaje),
-              fill = "#006765") +
+  ggplot(pobreza_tidy, aes(anio, porcentaje)) +
+  geom_area(fill = "#243C5C") +
   geom_vline(aes(xintercept = anio), 
              size = 0.1, 
-             color = "#f2efe6") +
-  facet_wrap( ~ entidad, scales = "free_x") +
-  scale_x_continuous(breaks = seq(2008, 2018, by = 2), labels = c("2008", "'10", "'12", "'14", "'16", "'18")) +
+             color = "gray80") +
+  facet_wrap( ~ entidad,
+              scales = "free_x") +
+  scale_x_continuous(breaks = seq(2008, 2018, by = 2),
+                     labels = c("2008", "'10", "'12", "'14", "'16", "'18")) +
   labs(title = "Diez años de combate a la pobreza en México",
        subtitle = "Porcentaje de personas en situación de pobreza por entidad federativa",
        caption = "Fuente: CONEVAL",
        x = "Año",
        y = "Porcentaje") +
-  theme_minimal(base_size = 7, base_family = "Roboto Condensed Light") +
-  theme(axis.title = element_text(hjust = 1),
-        panel.grid = element_line(colour = "#e6e0d0"),
-        panel.grid.minor = element_blank(),
+  theme_ybn_w(base_size = 9,
+              base_family = "Roboto Condensed Light",
+              title_hjust = 0.5,
+              subtitle_hjust = 0.5,
+              subtitle_margin_b = 25,
+              plot_margin = margin(25, 20, 20, 20)
+              ) +
+  theme(panel.spacing.x = unit(1, "lines"),
         panel.spacing.y = unit(1, "lines"),
-        plot.background = element_rect(fill = "#f2efe6",
-                                       color = "#f2efe6"),
-        plot.title = element_text(family = "Roboto Condensed",
-                                  colour = "#0f0e14",
-                                  size = 14,
-                                  margin = margin(t = 5, b = 5)),
-        plot.subtitle = element_text(size = 8,
-                                     margin = margin(b = 10)),
-        strip.text = element_text(face = "bold",
-                                  size = 8))
-  
+        panel.grid.minor = element_blank(),
+        strip.text = element_text(size = 8.5))
 
-ggsave("dia_04/04_facetas.png", p1, height = 7, width = 5.5, units = "in", dpi = 300)
-
-
-
-
-
+ggsave("dia_04/04_facetas2.png", p1, height = 11, width = 8.5, units = "in", dpi = 300)
