bfa1a4f2b4afec6ea34b8b8ec045c727a263d0f1
Yobanny Samano <yobanny_sc@hotmail.com>
2020-08-01 00:56:44 -0500

    commit

--- dia_14/dia14_treemap.R
+++ dia_14/dia14_treemap.R
@@ -10,21 +10,22 @@ petroleo <- petroleo %>%
          etiqueta = str_c(pais, "\n", barriles)) %>% 
   filter(barriles_millones > 1000)
 
-p1 <- ggplot(petroleo, aes(area = barriles_millones,
-                           label = etiqueta))+
-  geom_treemap(colour = "gray90", 
+p1 <- 
+  ggplot(petroleo, aes(area = barriles_millones,
+                       label = etiqueta))+
+  geom_treemap(colour = "#FDFDFB", 
                fill = "#131423", 
                size = 1.5) +
-  geom_treemap_text(colour = "white", 
+  geom_treemap_text(colour = "#FDFDFB", 
                     place = "topleft", 
                     min.size = 0, 
                     family = "Roboto Condensed Light",
                     reflow = T,
-                    size = 8) +
+                    size = 12) +
   labs(title = "Los países con mayores reservas de petróleo, en millones de barriles",
        subtitle = "Países con reservas superiores a 1,000 millones de barriles",
        caption = "Fuente: The World Factbook - CIA") +
-  theme_ybn(colour_background = "white")
+  theme_ybn_w()
 
-ggsave("dia_14/14_treemap.png", p1, height = 5, width = 8, units = "in", dpi = 300)
+ggsave("dia_14/14_treemap.png", p1, height = 8.5, width = 11, units = "in", type = "cairo")
 
