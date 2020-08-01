1d5afb91a1acea5f9ca082fff86c72c04d022b6b
Yobanny Samano <yobanny_sc@hotmail.com>
2020-08-01 00:02:36 -0500

    coomit

--- dia_09/dia_09_area.R
+++ dia_09/dia_09_area.R
@@ -21,32 +21,39 @@ nota <- "Nota: La muerte materna es definida como la muerte de una mujer embaraz
         atención pero no por causas accidentales o incidentales.
         <br>Fuente: Banco Mundial"
 
-colores <- c("#79788a", "#f283a4", "#3f3e6e", "#ec630f", "#d6146b", "#ffc50c", "#014a81")
+colores <- c("#79788a", "#905F4F", "#82A2BA", "#E8724A", "#699478", "#EBD991", "#56658C")
 
 
-x11()
-ggplot(muertes_mat, aes(anio, num_muertes, fill = country_code)) +
-  geom_area() +
-  scale_y_continuous(labels = scales::comma, n.breaks = 10, expand = c(0.01, 0)) +
-  scale_x_continuous(n.breaks = 10, expand = c(0.01, 0)) +
-  scale_fill_manual(values = colores, guide = guide_legend(nrow = 1), labels = c("América del Norte",
-                                                                                 "Europa y Asia Central",
-                                                                                 "Medio Oriente y Norte de África",
-                                                                                 "América Latina y el Caribe",
-                                                                                 "Asia Oriental y el Pacífico",
-                                                                                 "Asia del Sur",
-                                                                                 "África Subsahariana")) +
+p1 <- ggplot(muertes_mat) +
+  geom_area(aes(anio, num_muertes, fill = country_code)) +
+  scale_y_continuous(labels = scales::comma,
+                     n.breaks = 10,
+                     expand = c(0.01, 0)) +
+  scale_x_continuous(n.breaks = 10,
+                     expand = c(0.01, 0)) +
+  scale_fill_manual(values = colores,
+                    guide = guide_legend(nrow = 1),
+                    labels = c("América del Norte",
+                               "Europa y Asia Central",
+                               "Medio Oriente y Norte de África",
+                               "América Latina y el Caribe",
+                               "Asia Oriental y el Pacífico",
+                               "Asia del Sur",
+                               "África Subsahariana")) +
   labs(title = "Evolución de muertes maternas en el mundo",
        subtitle = "Número de muertes maternas por region, 2000-2017",
        caption = nota) +
-  theme_ybn() +
+  theme_ybn_w() +
   theme(axis.title = element_blank(),
-        axis.text = element_text(size = 5),
         legend.position = "top",
-        legend.key.size = unit(0.2, "cm"),
-        legend.text = element_text(size = 5),
+        legend.key.size = unit(0.35, "cm"),
+        legend.text = element_text(size = 8),
         legend.title = element_blank(),
-        plot.caption = element_markdown(size = 5,hjust = 0))
-
-ggsave("dia_09/09_area.png", height = 4.5, width = 7, units = "in", dpi = 300)
-
+        plot.caption = element_markdown(size = 8,
+                                        hjust = 0))
+ggsave(filename = "dia_09/09_area.png",
+       plot = p1, 
+       type = "cairo",
+       height = 8.5,
+       width = 11,
+       units = "in")
